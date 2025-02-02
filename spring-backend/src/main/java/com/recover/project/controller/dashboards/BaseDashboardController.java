4867dfbd5d5782fe5c40787469676f11a7ac38b9
grandpopsmasterson <dave@recoverdynamics.com>
2025-02-01 18:14:43 -0600

    controller changes

--- spring-backend/src/main/java/com/recover/project/controller/dashboards/BaseDashboardController.java
+++ spring-backend/src/main/java/com/recover/project/controller/dashboards/BaseDashboardController.java
@@ -1,44 +1,44 @@
 package com.recover.project.controller.dashboards;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.recover.project.utils.constants.Constants;
+import com.recover.project.dto.project.ShortProjectDto;
+import com.recover.project.model.enums.ProjectRole;
+import com.recover.project.service.authorization.AuthenticationService;
+import com.recover.project.service.project.ProjectService;
+import com.recover.project.service.roles.UserService;
+import com.recover.project.utils.exceptions.UnauthorizedException;
+
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
-
 @RestController
+@RequiredArgsConstructor
 public class BaseDashboardController {
-
     private final ProjectService projectService;
+    private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    @Autowired
-    public BaseDashboardController(ProjectService projectService) {
-        this.projectService = projectService;
-    }
-    
-    @GetMapping("/dashboard") // get all user's projects
+    @GetMapping("/dashboard")
     public ResponseEntity<?> getProjectById() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null) {
-            try {
-                UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-                Long userId = userDetails.getId();
-                List<Project> projects = projectService.getAllProjectsForUser(userId);
-                if (projects.isEmpty()) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Projects not found or not authorized");
-                }
-                return ResponseEntity.ok(projects);
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request");
-            }
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
-    }
+        try {
+            Long userId = authenticationService.getCurrentUserId();
+            ProjectRole currentUserRole = Constants.ROLE_MAP.get(userService.findById(userId).getUserType());
 
+            List<ShortProjectDto> projects = projectService.getProjectsListByRole(userId, currentUserRole);
+            if (projects.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Projects not found or not authorized");
+            }
+            return ResponseEntity.ok(projects);
+        } catch (UnauthorizedException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request");
+        }
+    }
 }
