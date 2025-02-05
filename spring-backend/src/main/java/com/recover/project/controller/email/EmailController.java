45f0684b79873e97181d216dce4f569caaeaa7bb
Will Carron <will@recover.systems>
2025-02-05 14:34:13 -0700

    Create password reset methods and service.

--- spring-backend/src/main/java/com/recover/project/controller/email/EmailController.java
+++ spring-backend/src/main/java/com/recover/project/controller/email/EmailController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recover.project.model.User;
+import com.recover.project.service.roles.UserService;
+import com.recover.project.utils.exceptions.ResourceNotFoundException;
 import com.recover.project.service.email.EmailService;
 
 @RestController
 @RequestMapping("/api/v1/emails")
 public class EmailController {
     private final EmailService emailService;
+    private UserService userService;
 
     public EmailController(
         @Autowired EmailService emailService
@@ -22,8 +25,14 @@ public class EmailController {
     }
 
     @PostMapping("/account-creation")
-    public ResponseEntity<String> sendAccountCreationEmail(User user)
- {
-    emailService.sendAccountCreationEmail(user);
-    return ResponseEntity.ok("Account Creation Email Sent Successfully");
- }}
+    public ResponseEntity<String> sendAccountCreationEmail(
+        @RequestBody String to
+    ) {
+        User user = userService.findByEmail(to).orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + to));
+        if (user != null) {
+            emailService.sendAccountCreationEmail(user);
+        }
+
+        return ResponseEntity.ok("Account Creation Email Sent Successfully.");
+    }
+}
---
+++ spring-backend/src/main/java/com/recover/project/service/authorization/RecoveryService.java
@@ -0,0 +1,8 @@
+package com.recover.project.service.authorization;
+import com.recover.project.model.User;
+
+public class RecoveryService {
+    public void generateRecoveryLink(User user) {
+
+    }    
+}
