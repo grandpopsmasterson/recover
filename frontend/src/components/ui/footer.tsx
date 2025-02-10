04199f3f2861a97f1da4aca0d344da457a534c0e
Will Carron <will@recover.systems>
2025-02-10 16:50:17 -0700

    footer links and css

--- frontend/src/components/ui/footer.tsx
+++ frontend/src/components/ui/footer.tsx
@@ -1,8 +1,18 @@
+import { Link } from "@heroui/link";
 export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <div className="footer w-full flex flex-col content-center items-center p-10">
             <h6>Copyright Recover Systems @{currentYear}</h6>
+            <div className="footer--links-container flex w-50 py-5">
+                <div className="footer--list flex w-full justify-around">
+                    <Link className="mx-2 text-primary" href="/about">About Us</Link>
+                    <Link className="mx-2 text-primary" href="/contact">Contact Us</Link>
+                    <Link className="mx-2 text-primary" href="/privacy">Privacy Policy</Link>
+                    <Link className="mx-2 text-primary" href="terms-of-service">Terms of Service</Link>
+                    <Link className="mx-2 text-primary" href="/legal">Legal</Link>
+                </div>         
+            </div>
         </div>
     )
 }
